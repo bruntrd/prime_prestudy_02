49705f00c6aac8cfc17aa9c7a2d021e77a6eeda5
bruntrd <bruntrd@gmail.com>
2015-07-06 19:55:07 -0500

    Initial Commit

---
+++ ObjectAssessment.js
@@ -0,0 +1,13 @@
+avengersMovie = {
+	name : Avengers;
+	runtime : 143 mins;
+	releaseYear = 2012;
+	"Director" : "Joss Whedon".
+}
+
+var avengersMovie = {
+	name : "Avengers",
+	runtime : "143 mins",
+	releaseYear : 2012,
+	Director : "Josh Whedon"
+}
